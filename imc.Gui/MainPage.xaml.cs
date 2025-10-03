64267a13be17b8195d4ec5c435d3a0c7daab28cd
José Flores <24470240@campeche.tecnm.mx>
2025-10-03 10:35:28 -0600

    Agrega la interfas grafica de usuario, sin comportamiento

---
+++ imc.Gui/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace imc.Gui
+{
+    public partial class MainPage : ContentPage
+    {
+       
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
